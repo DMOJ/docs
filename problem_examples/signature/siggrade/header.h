728a0a141640c4a518f866ab247c17f2a5fa5f12
Tudor Brindus <tbrindus@gmail.com>
2015-09-12 19:54:48 -0400

    Update header.h

--- problem_examples/signature/siggrade/header.h
+++ problem_examples/signature/siggrade/header.h
@@ -1,3 +1,8 @@
+/** 
+ * Header file for the signature grader
+ * A submission to this problem automatically has this header prepended to the source.
+ * It can then with the C grader (handler.c) via is_valid.
+ **/
 #ifndef _GRADER_HEADER_INCLUDED
 #define _GRADER_HEADER_INCLUDED
 #include <stdbool.h>
