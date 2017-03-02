09583abd4d0478e2044283761fd623960df65400
BinAccel <BinAccel@users.noreply.github.com>
2017-03-01 23:22:20 -0500

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -144,12 +144,10 @@ TERMS_OF_SERVICE_URL = '//dmoj.ca/tos' # Use a flatpage.
 # The judge connection address and port; where the judges will connect to the site.
 # You should change this to something your judges can actually connect to 
 # (e.g., a port that is unused and unblocked by a firewall).
-BRIDGED_JUDGE_HOST = 'localhost'
-BRIDGED_JUDGE_PORT = 9999
+BRIDGED_JUDGE_ADDRESS = [('localhost', 9999)]
 
 # The bridged daemon bind address and port to communicate with the site.
-#BRIDGED_DJANGO_HOST = 'localhost'
-#BRIDGED_DJANGO_PORT = 9998
+#BRIDGED_DJANGO_ADDRESS = [('localhost', 9998)]
 
 ## DMOJ features.
 # Set to True to enable full-text searching for problems.
